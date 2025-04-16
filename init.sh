ea3b0ac3ead32104643021490447b4bb6467c514
Your Name <you@example.com>
2025-04-16 22:20:40 +0300

    basic tweaks and setup

--- init.sh
+++ init.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#  Copyright 2025 Roman Kudinov. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 cd $(dirname "$0")
 
 if [ "$#" -ne 2 ]; then
@@ -25,7 +41,10 @@ if [[ $METHOD == "-f" ]]; then
     echo $MANIFEST_TEXT
 elif [[ $METHOD == "-u" ]]; then
     echo "cloning git repos listed in manifest url" $MANIFEST_PATH
-    MANIFEST_TEXT=$(curl "$MANIFEST_PATH" 2>/dev/null)
+    if ! MANIFEST_TEXT=$(curl -fsSL "$MANIFEST_PATH"); then
+        echo "Failed to fetch manifest from $MANIFEST_PATH"
+        exit 1
+    fi
     echo $MANIFEST_TEXT
 else
     echo "bad method " $METHOD
--- status.sh
+++ status.sh
@@ -2,6 +2,10 @@
 
 cd $(dirname "$0")
 
+echo "Status for root workspace:"
+(git status)
+echo "------------------------------------"
+    
 for i in src/*;
 do
     echo "Status for: $i:"
